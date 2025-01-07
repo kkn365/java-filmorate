dc3f20143f26fa3b12771395c39a1b87a1affdd7
Konstantin <KvitchenkoKN@yandex.ru>
2025-01-08 00:29:24 +0300

    Исправление замечаний автотестов github.

--- src/main/java/ru/yandex/practicum/filmorate/exception/InternalServerException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/InternalServerException.java
@@ -2,7 +2,8 @@ package ru.yandex.practicum.filmorate.exception;
 
 public class InternalServerException extends RuntimeException {
 
-    public InternalServerException() {}
+    public InternalServerException() {
+    }
 
     public InternalServerException(String message) {
         super(message);
