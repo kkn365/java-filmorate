0de9e0164f2b717b587e7fe209c4a2202320c881
AkadiG1t <dubkowalex@yandex.ru>
2025-01-30 00:06:07 +0300

    Добавить новый функционал: вывод общих с другом фильмов

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -8,22 +8,14 @@ import ru.yandex.practicum.filmorate.dal.film.FilmStorage.FilmStorage;
 import ru.yandex.practicum.filmorate.dto.*;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
-import ru.yandex.practicum.filmorate.mapper.DirectorMapper;
 import ru.yandex.practicum.filmorate.mapper.FilmMapper;
 import ru.yandex.practicum.filmorate.mapper.GenreMapper;
 import ru.yandex.practicum.filmorate.mapper.MPAMapper;
-import ru.yandex.practicum.filmorate.model.Director;
-import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.Genre;
-import ru.yandex.practicum.filmorate.model.Like;
-import ru.yandex.practicum.filmorate.model.MPA;
+import ru.yandex.practicum.filmorate.model.*;
 
 import java.util.Collection;
-import java.util.Comparator;
-import java.util.LinkedHashSet;
-import java.util.List;
+import java.util.Optional;
 import java.util.Set;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -35,151 +27,130 @@ public class FilmService {
     private final UserService userService;
     private final MpaService mpaService;
     private final GenreService genreService;
-    private final DirectorService directorService;
 
-    private static final Comparator<FilmDto> comparatorByYear = Comparator.comparing(FilmDto::getReleaseDate);
-    private static final Comparator<FilmDto> comparatorByLikes = Comparator.comparing(FilmDto::getLiked).reversed();
+    public Collection<FilmDto> getFilms() {
+        Collection<Film> films = filmStorage.getAllFilms();
+        Collection<MpaDto> mpas = mpaService.getAllMPAs();
+        Collection<FilmGenresDto> allFilmsGenres = genreService.getAllFilmsGenres();
 
-    public FilmDto getFilmById(Long id) {
-        Film film = filmStorage.getFilmById(id);
-        if (film == null) {
-            final String warnMessage = String.format("Фильм c id=%d не найден в базе данных.", id);
-            log.warn(warnMessage);
-            throw new NotFoundException(warnMessage);
+        for (Film film : films) {
+
+            final String mpaName = mpas.stream()
+                    .filter(mpa -> mpa.getId().equals(film.getMpa().getId()))
+                    .findFirst()
+                    .map(MpaDto::getName)
+                    .get();
+            film.getMpa().setName(mpaName);
+
+            final Set<Genre> genres = allFilmsGenres
+                    .stream()
+                    .filter(genre -> genre.getFilmId().equals(film.getId()))
+                    .map(genre -> Genre.builder()
+                            .id(genre.getGenreId())
+                            .name(genre.getGenreName())
+                            .build())
+                    .collect(Collectors.toSet());
+            film.setGenres(genres);
+
         }
 
-        final int mpaId = film.getMpa().getId();
-        final MPA filmMpa = MPAMapper.mapToMPA(mpaService.getMPAbyId(mpaId));
-        film.setMpa(filmMpa);
-
-        final Set<Genre> filmGenres = genreService.getFilmGenresByFilmId(id)
+        return films
                 .stream()
-                .map(GenreMapper::mapToGenre)
-                .collect(Collectors.toSet());
-        film.setGenres(filmGenres);
-
-        final Set<Director> filmDirectors = directorService.getFilmDirectorByFilmId(id)
-                .stream()
-                .map(DirectorMapper::mapToDirector)
-                .collect(Collectors.toSet());
-        film.setDirectors(filmDirectors);
-
-        return FilmMapper.mapToFilmDto(film);
-    }
-
-    public Collection<FilmDto> getAllFilms() {
-        final Collection<Film> allFilms = filmStorage.getAllFilms();
-        final Collection<MpaDto> allMPAs = mpaService.getAllMPAs();
-        final Collection<FilmGenresDto> allGenres = genreService.getAllFilmsGenres();
-        final Collection<DirectorFilmsDto> allDirectors = directorService.getAllFilmDirectors();
-
-        return allFilms.stream()
-                .peek(film -> {
-                            film.setMpa(allMPAs.stream()
-                                    .filter(mpa -> mpa.getId().equals(film.getMpa().getId()))
-                                    .findFirst()
-                                    .map(MPAMapper::mapToMPA)
-                                    .orElse(null)
-                            );
-                            film.setGenres(allGenres.stream()
-                                    .filter(genre -> genre.getFilmId().equals(film.getId()))
-                                    .map(genre -> Genre.builder()
-                                            .id(genre.getGenreId())
-                                            .name(genre.getGenreName())
-                                            .build())
-                                    .collect(Collectors.toSet())
-                            );
-                            film.setDirectors(allDirectors.stream()
-                                    .filter(director -> director.getFilmId().equals(film.getId()))
-                                    .map(director -> Director.builder()
-                                            .id(director.getDirectorId())
-                                            .name(director.getDirectorName())
-                                            .build())
-                                    .collect(Collectors.toSet())
-                            );
-                        }
-                )
                 .map(FilmMapper::mapToFilmDto)
                 .collect(Collectors.toList());
     }
 
     public FilmDto addNewFilm(FilmDto film) {
-        FilmDto newFilm = FilmMapper.mapToFilmDto(filmStorage.addNewFilm(FilmMapper.mapToFilm(film)));
-        if (!newFilm.getGenres().isEmpty()) {
-            final Set<Integer> incomingGenresIds = newFilm.getGenres()
+
+        final Integer mpaId = film.getMpa().getId();
+        if (mpaId != null) {
+            mpaService.getMPAbyId(mpaId);
+        }
+
+        if (film.getGenres() != null) {
+
+            final Set<Genre> incomingFilmGenres = film.getGenres();
+            final Collection<GenreDto> currentFilmGenres = genreService.getAllGenres();
+
+            final Set<Integer> currentFilmGenresIds = currentFilmGenres
                     .stream()
                     .map(GenreDto::getId)
                     .collect(Collectors.toSet());
-            final Set<GenreDto> incomingGenres = genreService.getAllGenres()
+
+            final Set<Integer> incomingFilmGenresIds = incomingFilmGenres
                     .stream()
-                    .filter(genre -> incomingGenresIds.contains(genre.getId()))
+                    .map(Genre::getId)
                     .collect(Collectors.toSet());
-            newFilm.setGenres(incomingGenres);
-            genreService.addGenresToFilm(newFilm);
+
+            for (Integer genreId : incomingFilmGenresIds) {
+                if (!currentFilmGenresIds.contains(genreId)) {
+                    log.warn("Не найден жанр с id={}", genreId);
+                    throw new NotFoundException(String.format("Жанр с id=%d не найден.", genreId));
+                }
+            }
+
         }
-        if (!newFilm.getDirectors().isEmpty()) {
-            final Set<Long> incomingDirectorsIds = newFilm.getDirectors()
-                    .stream()
-                    .map(DirectorDto::getId)
-                    .collect(Collectors.toSet());
-            final Set<DirectorDto> incomingDirectors = directorService.getAllDirectors()
-                    .stream()
-                    .filter(director -> incomingDirectorsIds.contains(director.getId()))
-                    .collect(Collectors.toSet());
-            newFilm.setDirectors(incomingDirectors);
-            directorService.addDirectorsToFilm(newFilm);
-        }
-        log.info("Добавлен новый фильм: {}", newFilm);
-        return getFilmById(newFilm.getId());
+
+        Film newFilm = filmStorage.addNewFilm(FilmMapper.mapToFilm(film));
+
+        return FilmMapper.mapToFilmDto(newFilm);
     }
 
     public FilmDto updateFilmData(FilmDto film) {
         final Long id = film.getId();
+
         if (id == null) {
             log.warn("Не указан id в теле запроса: {}", film);
             throw new ValidationException("Должен быть указан id фильма.");
         }
-        getFilmById(id);
-        filmStorage.updateCurrentFilm(FilmMapper.mapToFilm(film));
-        genreService.deleteGenresFromFilm(id);
-        if (film.getGenres() != null) {
-            genreService.addGenresToFilm(film);
+
+        if (filmStorage.getFilmById(id) == null) {
+            log.warn("Не найден фильм с id={}", id);
+            throw new NotFoundException(String.format("Фильм с id=%d не найден.", id));
         }
-        directorService.deleteDirectorsFromFilm(id);
-        if (film.getDirectors() != null) {
-            directorService.addDirectorsToFilm(film);
-        }
-        FilmDto updatedFilm = getFilmById(id);
-        log.info("Обновлены данные фильма: {}", updatedFilm);
-        return updatedFilm;
+
+        Film updatedFilm = filmStorage.updateCurrentFilm(FilmMapper.mapToFilm(film));
+
+        return FilmMapper.mapToFilmDto(updatedFilm);
     }
 
-    public String putLike(Long filmId, Long userId) {
-        getFilmById(filmId);
+    public FilmDto putLike(Long filmId, Long userId) {
+        filmStorage.getFilmById(filmId);
         userService.getUserById(userId);
-        filmStorage.putLike(filmId, userId);
-        final String message = String.format("Пользователь с id=%d поставил лайк фильму с id=%d.", userId, filmId);
-        log.info(message);
-        return message;
+        Film likedFilm = filmStorage.putLike(filmId, userId);
+        return FilmMapper.mapToFilmDto(likedFilm);
     }
 
-    public String deleteLike(Long filmId, Long userId) {
-        getFilmById(filmId);
+    public FilmDto deleteLike(Long filmId, Long userId) {
+        filmStorage.getFilmById(filmId);
         userService.getUserById(userId);
-        filmStorage.deleteLike(filmId, userId);
-        final String message = String.format("Пользователь с id=%d удалил лайк фильму с id=%d.", userId, filmId);
-        log.info(message);
-        return message;
+        Film deletedLikeFilm = filmStorage.deleteLike(filmId, userId);
+        return FilmMapper.mapToFilmDto(deletedLikeFilm);
     }
 
-    public Collection<FilmDto> getPopular(Integer count, Integer genreId, Integer year) {
-        final Set<Long> popularFilmsIds = filmStorage.getPopularFilms(count, genreId, year).stream()
-                .map(Film::getId)
+    public Collection<FilmDto> getPopular(Integer count) {
+        return filmStorage.getPopularFilms(count)
+                .stream()
+                .map(FilmMapper::mapToFilmDto)
+                .collect(Collectors.toList());
+    }
+
+    public FilmDto getFilmById(Long id) {
+
+        Film film = filmStorage.getFilmById(id);
+
+        final Set<Genre> filmGenres = genreService.getFilmGenresByFilmId(id)
+                .stream()
+                .map(GenreMapper::mapToGenre)
                 .collect(Collectors.toSet());
-        return getAllFilms().stream()
-                .filter(film -> popularFilmsIds.contains(film.getId()))
-                .sorted(Comparator.comparing(FilmDto::getId))
-                .toList();
+
+        film.setGenres(filmGenres);
+
+        final int mpaId = film.getMpa().getId();
+        final MPA filmMpa = MPAMapper.mapToMPA(mpaService.getMPAbyId(mpaId));
+        film.setMpa(filmMpa);
+
+        return FilmMapper.mapToFilmDto(film);
     }
 
     public Collection<Like> getDataField(Long userId) {
@@ -190,55 +161,39 @@ public class FilmService {
         if (userId == null || userId <= 0 || friendId == null || friendId <= 0) {
             throw new ValidationException("ID пользователя должно быть выше 0");
         }
-        userService.getUserById(userId);
-        userService.getUserById(friendId);
-        final Collection<Long> filmsIds = filmStorage.getCommonFilmsWithFriend(userId, friendId)
-                .stream()
-                .map(Film::getId)
-                .sorted()
-                .toList();
-        return getAllFilms()
-                .stream()
-                .filter(film -> filmsIds.contains(film.getId()))
-                .sorted(Comparator.comparingLong(FilmDto::getLiked).reversed())
-                .collect(Collectors.toCollection(LinkedHashSet::new));
-    }
 
-    public String deleteFilmById(Long filmId) {
-        getFilmById(filmId);
-        filmStorage.deleteFilmById(filmId);
-        final String message = String.format("Удален фильм с id=%d.", filmId);
-        log.info(message);
-        return message;
-    }
+        UserDto user = Optional.ofNullable(userService.getUserById(userId))
+                .orElseThrow(() -> new NotFoundException("Пользователь с ID " + userId + " не найден"));
+        UserDto friend = Optional.ofNullable(userService.getUserById(friendId))
+                .orElseThrow(() -> new NotFoundException("Пользователь с ID " + friendId + " не найден"));
 
-    public Collection<FilmDto> searchFilms(String query, List<String> by) {
-        final Pattern pattern = Pattern.compile(query, Pattern.CASE_INSENSITIVE);
-        return getAllFilms()
-                .stream()
-                .filter(film -> {
-                    if (by.contains("title") && (pattern.matcher(film.getName()).find())) return true;
-                    if (by.contains("director")) {
-                        return film.getDirectors()
-                                .stream()
-                                .map(DirectorDto::getName)
-                                .anyMatch(name -> pattern.matcher(name).find());
-                    }
-                    return false;
-                })
-                .sorted(Comparator.comparingLong(FilmDto::getLiked).reversed())
-                .collect(Collectors.toCollection(LinkedHashSet::new));
-    }
+        Collection<Film> films = filmStorage.getCommonFilmsWithFriend(userId, friendId);
+        Collection<MpaDto> mpas = mpaService.getAllMPAs();
+        Collection<FilmGenresDto> allFilmsGenres = genreService.getAllFilmsGenres();
 
-    public Collection<FilmDto> getFilmsByDirectors(Long directorId, String sortBy) {
-        directorService.getDirectorById(directorId);
-        final Comparator<FilmDto> comparator = sortBy.equals("year") ? comparatorByYear : comparatorByLikes;
-        return getAllFilms()
+        for (Film film : films) {
+            final String mpaName = mpas.stream()
+                    .filter(mpa -> mpa.getId().equals(film.getMpa().getId()))
+                    .findFirst()
+                    .map(MpaDto::getName)
+                    .get();
+            film.getMpa().setName(mpaName);
+
+
+            final Set<Genre> genres = allFilmsGenres
+                    .stream()
+                    .filter(genre -> genre.getFilmId().equals(film.getId()))
+                    .map(genre -> Genre.builder()
+                            .id(genre.getGenreId())
+                            .name(genre.getGenreName())
+                            .build())
+                    .collect(Collectors.toSet());
+            film.setGenres(genres);
+        }
+
+        return films
                 .stream()
-                .filter(film -> film.getDirectors()
-                        .stream()
-                        .anyMatch(directorDto -> directorDto.getId().equals(directorId)))
-                .sorted(comparator)
-                .collect(Collectors.toCollection(LinkedHashSet::new));
+                .map(FilmMapper::mapToFilmDto)
+                .collect(Collectors.toList());
     }
 }
