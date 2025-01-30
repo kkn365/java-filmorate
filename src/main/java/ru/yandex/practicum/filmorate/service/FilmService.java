09c71bb652e555363a7305f23608ba20e0bbcce8
AkadiG1t <dubkowalex@yandex.ru>
2025-01-30 13:01:35 +0300

    Исправить название ветки

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -179,7 +179,6 @@ public class FilmService {
                     .get();
             film.getMpa().setName(mpaName);
 
-
             final Set<Genre> genres = allFilmsGenres
                     .stream()
                     .filter(genre -> genre.getFilmId().equals(film.getId()))
