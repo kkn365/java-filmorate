67e48ea659506ee8f7efd181b163a50c4d02ed17
Ivan Korolkov <nerounxqxq@gmail.com>
2025-02-04 00:07:46 +0400

    Исправлена сортировка отзывов

--- src/main/java/ru/yandex/practicum/filmorate/dal/review/reviewStorage/ReviewDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/review/reviewStorage/ReviewDbStorage.java
@@ -47,7 +47,7 @@ public class ReviewDbStorage implements ReviewStorage {
             SELECT *
             FROM reviews
             WHERE film_id = ?
-            ORDER BY USEFUL
+            ORDER BY USEFUL DESC
             LIMIT ?
             """;
     private static final String GET_ALL_REVIEWS = """
@@ -100,9 +100,10 @@ public class ReviewDbStorage implements ReviewStorage {
 
     @Override
     public List<Review> getReviewsByFilm(Long filmId, Integer count) {
-        if (filmId == -1) {
+        if (filmId == 0) {
             return jdbcTemplate.query(GET_ALL_REVIEWS, reviewMapper, count);
         }
+
         return jdbcTemplate.query(GET_REVIEW_BY_FILM, reviewMapper, filmId, count);
     }
 }
